6c419d3c5f145a37ff64902bc29e2d990a092f9c
Bahlbi Brhane Gebrealif <bahlbibg@gmail.com>
2021-12-05 14:21:23 -0500

    global variables added

--- deuhosp.c
+++ deuhosp.c
@@ -4,3 +4,20 @@
 #include <time.h>
 #include <semaphore.h>
 #include <stdbool.h>
+
+//GLOBAL VARIABLES
+int REGISTRATION_SIZE = 10; //max registration desks available.
+int RESTROOM_SIZE = 10; //max restrooms that are available.
+int CAFE_NUMBER = 10; //max cashiers in cafe that are available.
+int GP_NUMBER = 10; //max General Practitioner that are available.
+int PHARMACY_NUMBER = 10; //max cashiers in pharmacy that are available.
+int BLOOD_LAB_NUMBER = 10; //max assistants in blood lab that are available.
+int OR_NUMBER = 10; //max operating rooms
+int SURGEON_NUMBER = 30; //max surgeons
+int NURSE_NUMBER = 30; //max nurses that are available.
+
+
+int main(int argc, char *argv[])
+{
+
+}
